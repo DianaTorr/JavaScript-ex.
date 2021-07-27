b7862d6f069ba5cf3ccff06bcb74a712c23c8904
Diana Torres <87824448+DianaTorr@users.noreply.github.com>
2021-07-27 17:46:42 +0000

    .

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-var lives;
-lives=2;
- 
- var score;
- score=3;
+main();
+function main(){
+runningCalculator();
+var playAgain = prompt ("\n would you like to play the game again Y/N?");
 
- chairs= lives + score; console.log(lives);
+if(playAgain == "Y")
+{
+  main();
+}
+else
+{
+  console.log ("Thank you for finishing the game.");
+}
+}
+function runningCalculator(){
+
+var totalMiles = prompt ("How many miles did you run today?");
+totalMiles= parseFloat (totalMiles);
+
+var timePerMile = prompt("How long did it take to run 1 mile?");
+timePerMile=parseFloat (timePerMile)
+
+var totalTime=
+totalMiles* timePerMile;
+console.log("You spent " + totalTime + " minutes running")
+
+}
+
+
+
